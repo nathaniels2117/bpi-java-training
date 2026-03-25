7d9ef52b60f60230d74fa45f5346ada9d2dda810
“nathaniels2117” <nathanieldc27@gmail.com>
2026-03-25 14:46:01 +0800

    M9 Exercise 5

--- M9_Exercise5/src/main/java/com/bpi/traning/M9_module/config/SecurityConfig.java
+++ M9_Exercise5/src/main/java/com/bpi/traning/M9_module/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.bpi.traning.M9_module.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
